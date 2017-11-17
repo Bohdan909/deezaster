7544d1de20d92ec689a58f109002a2dcd50432c4
Bohdan <bohdan.bilous2@gmail.com>
2017-11-17 12:07:29 +0200

    commit 2

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,6 @@ window.addEventListener("load", ()=> {
     document.querySelector(".section-top").classList.add("loaded");
     document.querySelector(".navigation").classList.add("loaded");
 
-    // document.querySelectorAll(".section").forEach(function (item){
-    //     item.classList.add("loaded");
-    // });
-
 
     // Game Demo
     $cubeWanted.forEach(function(item){
@@ -23,10 +19,94 @@ window.addEventListener("load", ()=> {
         });
     });
 
+    // Map
+    let $map   = document.querySelector(".map-wrap");
+    let $hole  = document.querySelector(".hole");
+    let $monet = document.querySelectorAll(".monet");
+    let $desc  = document.querySelectorAll(".desc-block");
     
 
+    $monet.forEach(function(monet){
 
-    
+        monet.addEventListener("click", function(){
+
+            let index = this.getAttribute("data-index");
+
+            monet.style = "z-index: 0";
+            this.classList.add("hide");
+
+            $desc.forEach( (descItem) => {
+                descItem.classList.remove("show");
+            });
+
+            switch(index){
+                case "4":
+                    $map.style = "transform: translateY(-625px); -webkit-transform: translateY(-625px)";
+                    $hole.style = "top: 885px; left: 10px";
+                    monetFocus(1);
+                    
+                    document.querySelector(".desc-block[data-index='" + 3 + "'").classList.add("show");
+                break;
+
+                case "3":
+                    $map.style = "transform: translateY(-1000px); -webkit-transform: translateY(-1000px)";
+                    $hole.style = "top: 1165px; left: 455px";
+                    monetFocus(2);
+                    
+                    document.querySelector(".desc-block[data-index='" + 2 + "'").classList.add("show");
+                break;
+
+                case "2":
+                    $map.style = "transform: translateY(-1500px); -webkit-transform: translateY(-1500px)";
+                    $hole.style = "top: 1744px; left: 253px";
+                    monetFocus(3);
+
+                    document.querySelector(".desc-block[data-index='" + 1 + "'").classList.add("show");
+                break;
+
+                case "1":
+                    $map.style = "transform: translateY(-2130px); -webkit-transform: translateY(-2130px)";
+                    $hole.classList.add("hide");
+                    document.querySelector(".footer").classList.add("animate");
+                break;
+            }
+
+            function monetFocus(i){
+                setTimeout( () => {
+                        $monet[i].style = "z-index: 100";
+                    }, 950
+                );
+            }
+
+        });
+    });
+
+
+    // Tel Hover
+    let $btnDownload = document.querySelectorAll(".download");
+
+    $btnDownload.forEach(function(btn){
+        btn.addEventListener("mouseover", function(){
+            let index = this.getAttribute("data-index");
+
+            switch(index){
+                case "1":
+                    document.querySelector(".tel-2").classList.add("hover");
+                break;
+                
+                case "2":
+                    document.querySelector(".tel-1").classList.add("hover");
+                break;    
+            }
+        });
+
+        btn.addEventListener("mouseleave", function(){
+            document.querySelectorAll(".tel").forEach(function(tel){
+                tel.classList.remove("hover");
+            });
+        });
+    });
+
 
 
     winResize();
